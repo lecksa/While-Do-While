06db27127756a715a348f3bc0bc2ff496b848b9b
lecksa <dolabesipa@gmail.com>
2023-11-09 11:01:14 +0500

    homework

---
+++ index.js
@@ -0,0 +1,30 @@
+// let pwd
+
+// do{
+//     pwd = +prompt('password')
+// }while(pwd !== 7777)
+
+
+
+//while
+
+// let number = +prompt('')
+
+// let table = 0
+
+// while(table < 10){
+//     table++
+//     document.write(`${number} * ${table} = ${number * table} <br>`)
+// }
+
+
+//do while
+
+let number = +prompt('')
+
+let table = 0
+
+do{
+    table++
+    document.write(`${number} * ${table} = ${number * table} <br>`)
+}while(table < 10)
